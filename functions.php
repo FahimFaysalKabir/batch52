dd8a083213fa3983704db9815dc76ffe07748431
Fahim Faysal Kabir <githubbadhon@gmail.com>
2023-08-22 01:51:17 +0600

    SIMPLE WP THEME DONE
    1.index.php
    //some html structure codes
    2.style.css
    //null
    3.functions.php
    //add_theme_support
    etc

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+add_theme_support('menus');
+add_theme_support('widgets');
+add_theme_support('custom-header');
+add_theme_support('custom-background');
+add_theme_support('post-thumbnails');
+register_nav_menu( 'um', 'uporer menu' );
+register_nav_menu( 'nm', 'uporer menu' );
+register_nav_menu( 'mehedi', 'dan pasher menu' );
+?>
--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php bloginfo('title'); ?></title>
+
+
+    <?php wp_head();?>
 </head>
-<body>
-    
+<body <?php body_class();?>>
+  
+  <?php wp_nav_menu('um')?>
+ <?php wp_footer(); ?>
+ <?php while(have_posts()):the_post(); ?>
+ <?php the_post_thumbnail();?> 
+ <h2><?php the_title(); ?></h2>
+ <?php the_content(); ?>
+  <?php endwhile; ?>
+  ?>
+
 </body>
 </html>
